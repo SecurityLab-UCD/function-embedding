73c3f46e3ee24c34379da715a536c9c4304a6e2a
Yifeng He <yfhe@ucdavis.edu>
2022-12-11 14:15:46 -0800

    format to LLVM

--- encode_example.cpp
+++ encode_example.cpp
@@ -2,61 +2,59 @@
 
 using namespace std;
 
-int main(int argc, char** argv) {
-    printf("SCANF_ALT: float and int separate by space\n");
-    float a;
-    int b;
-    SCANF_ALT("%f %d", &a, &b);
+int main(int argc, char **argv) {
+  printf("SCANF_ALT: float and int separate by space\n");
+  float a;
+  int b;
+  SCANF_ALT("%f %d", &a, &b);
 
-    printf("SCANF_ALT: char and string separate by comma\n");
-    char c;
-    char d[64];
-    SCANF_ALT(" %c,%s", &c, d);
+  printf("SCANF_ALT: char and string separate by comma\n");
+  char c;
+  char d[64];
+  SCANF_ALT(" %c,%s", &c, d);
 
-    printf("SCANF_ALT: char as ptr+2\n");
-    char str[64];
-    SCANF_ALT(" %c", str+2);
+  printf("SCANF_ALT: char as ptr+2\n");
+  char str[64];
+  SCANF_ALT(" %c", str + 2);
 
-    printf("SCANF_ALT: long\n");
-    long li_var;
-    SCANF_ALT(" %ld", &li_var);
+  printf("SCANF_ALT: long\n");
+  long li_var;
+  SCANF_ALT(" %ld", &li_var);
 
-    printf("SCANF_ALT: ptr_var to short (enter short int)\n");
-    // this is also test by the string case
-    short si_var;
-    short* ptr_var = &si_var;
-    SCANF_ALT(" %hd", ptr_var);
+  printf("SCANF_ALT: ptr_var to short (enter short int)\n");
+  // this is also test by the string case
+  short si_var;
+  short *ptr_var = &si_var;
+  SCANF_ALT(" %hd", ptr_var);
 
-    string loop_msg = "Enter int, or exit with EOF (CTRL+D)\n";
-    int scanf_loop_buff;
+  string loop_msg = "Enter int, or exit with EOF (CTRL+D)\n";
+  int scanf_loop_buff;
+  cout << loop_msg;
+  while (SCANF_ALT(" %d", &scanf_loop_buff)) {
     cout << loop_msg;
-    while(SCANF_ALT(" %d", &scanf_loop_buff)) {
-      cout << loop_msg;
-    }
+  }
 
+  cout << "CIN int\n";
+  int e;
+  CIN(e);
 
+  cout << "CIN float\n";
+  float f;
+  CIN(f);
 
-    cout << "CIN int\n";
-    int e;
-    CIN(e);
+  cout << "CIN char\n";
+  char g;
+  CIN(g);
 
-    cout << "CIN float\n";
-    float f;
-    CIN(f);
+  cout << "CIN string\n";
+  std::string h;
+  CIN(h);
 
-    cout << "CIN char\n";
-    char g;
-    CIN(g);
-
-    cout << "CIN string\n";
-    std::string h;
-    CIN(h);
-
-    int cin_loop_buff;
+  int cin_loop_buff;
+  cout << loop_msg;
+  while (CIN(cin_loop_buff)) {
     cout << loop_msg;
-    while (CIN(cin_loop_buff)) {
-      cout << loop_msg;
-    }
+  }
 
-    return 0;
+  return 0;
 }
