43963e26cfd12f43f59284850cab94e64e2fba63
Peter Rong <PeterRong96@gmail.com>
2022-12-11 20:23:09 -0800

    split two dataset
    Signed-off-by: Peter Rong <PeterRong96@gmail.com>

--- scripts/build.py
+++ scripts/build.py
@@ -32,6 +32,11 @@ def compile_one_file(p: Tuple[str, str]):
 
 stderrs = []
 
+DATASETDIR = path.join(EMBDING_HOME, "IBM")
+SRCDIR = path.join(DATASETDIR, "src", "Project_CodeNet_C++1000")
+TXTDIR = path.join(DATASETDIR, "txt", "Project_CodeNet_C++1000")
+BINDIR = path.join(DATASETDIR, "build", "Project_CodeNet_C++1000")
+
 
 def dump_stderr_on_exit(p: subprocess.Popen):
     # TODO: Change fixed ./O output file.
@@ -39,35 +44,42 @@ def dump_stderr_on_exit(p: subprocess.Popen):
         f.write(p.stderr.read().decode())
 
 
-def mkdir_if_doesnt_exist(dir):
+def mkdir_if_doesnt_exist(dir, template=TXTDIR):
     if not path.isdir(dir):
         os.makedirs(dir)
-    for i in PROBLEMS:
+    for i in os.listdir(template):
         subdir = path.join(dir, str(i))
         if not path.isdir(subdir):
             os.makedirs(subdir)
 
 
-# TODO: replace CORES with command line arg.
-def compile_all(jobs: int = CORES, on_exit=None):
+def preprocess_all():
     if not path.isdir(TXTDIR):
         error(
             f"{TXTDIR} doesn't exist yet, please refer to README to download the dataset first."
         )
-    mkdir_if_doesnt_exist(BINDIR)
-    mkdir_if_doesnt_exist(SRCDIR)
-
-    # Copy the files and do some preprocessing
-    files_to_compile: List[Tuple[str, str]] = []
+    mkdir_if_doesnt_exist(SRCDIR, TXTDIR)
     info("Preprocessing text files into codes")
-    for i in tqdm(PROBLEMS):
+    for i in tqdm(os.listdir(TXTDIR)):
         for p in os.listdir(path.join(TXTDIR, str(i))):
             p = p[:-4]
             txt_path = path.join(TXTDIR, str(i), str(p) + ".txt")
             src_path = path.join(SRCDIR, str(i), str(p) + ".cpp")
-            bin_path = path.join(BINDIR, str(i), str(p))
             if not path.isfile(src_path):
                 preprocess_textfile(txt_path, src_path)
+
+
+# TODO: replace CORES with command line arg.
+def compile_all(jobs: int = CORES, on_exit=None):
+    mkdir_if_doesnt_exist(BINDIR, SRCDIR)
+    # Copy the files and do some preprocessing
+    files_to_compile: List[Tuple[str, str]] = []
+    info("Collecting codes to compile")
+    for i in tqdm(os.listdir(SRCDIR)):
+        for p in os.listdir(path.join(SRCDIR, str(i))):
+            p = p[:-4]
+            src_path = path.join(SRCDIR, str(i), str(p) + ".cpp")
+            bin_path = path.join(BINDIR, str(i), str(p))
             if not path.isfile(bin_path):
                 files_to_compile.append((src_path, bin_path))
 
@@ -78,6 +90,7 @@ def compile_all(jobs: int = CORES, on_exit=None):
 def main():
     logging.basicConfig()
     logging.getLogger().setLevel(logging.INFO)
+    # preprocess_all()
     compile_all(on_exit=dump_stderr_on_exit)
 
 
