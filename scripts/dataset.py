3fa1fd6c442b65c09781d4b2a350575cd87f46b3
Peter Rong <PeterRong96@gmail.com>
2022-12-20 14:31:21 -0800

    update dataset script.
    - move all preprocessing, compile, fuzzing, postprocessing together.
    - remove build.
    - add download script to IBM, POJ pending
    
    Signed-off-by: Peter Rong <PeterRong96@gmail.com>

--- scripts/dataset.py
+++ scripts/dataset.py
@@ -8,6 +8,20 @@ import subprocess
 import re
 from logging import error, info, warning
 from replace_input import replace_file
+import requests
+import tarfile
+import random
+import argparse
+
+
+def dump_stderr_on_exit(errfile: str, p: subprocess.Popen):
+    with open(errfile, "ab") as f:
+        try:
+            _, stderr = p.communicate(timeout=15)
+        except TimeoutExpired:
+            p.kill()
+            _, stderr = p.communicate()
+        f.write(stderr)
 
 
 def compile_one_file(p: Tuple[str, str]):
@@ -27,6 +41,18 @@ def compile_one_file(p: Tuple[str, str]):
     )
 
 
+def fuzz_one_file(p: Tuple[str, str], timeout: int, seeds: str):
+    bin, out = p
+    process = subprocess.Popen(
+        [f"{AFL}/afl-fuzz", "-V", str(timeout), "-i", seeds, "-o", out, bin],
+        stdout=subprocess.DEVNULL,
+        stderr=subprocess.DEVNULL,
+    )
+    # Sleep half a second for AFL to bind core.
+    subprocess.run(["sleep", "0.5"])
+    return process
+
+
 def format_one_file(src: str):
     return subprocess.Popen(
         [f"{LLVM}/bin/clang-format", src],
@@ -35,25 +61,31 @@ def format_one_file(src: str):
     )
 
 
+def coin_toss(percentage: float):
+    return random.random() <= percentage / 100.0
+
+
 class DataSet:
     def __init__(self, workdir, txtdir, language):
         self.workdir = path.abspath(workdir)
         self.txtdir = path.join(self.workdir, txtdir)
         self.srcdir = path.join(self.workdir, "src")
         self.bindir = path.join(self.workdir, "build")
+        self.outdir = path.join(self.workdir, "fuzz")
         self.lang = language
 
     def download(self):
         pass
 
     def preprocess_all(self):
-        if not path.isdir(self.txtdir):
-            # TODO: Call download here.
-            error(
-                f"{self.txtdir} doesn't exist yet, please refer to README to download the dataset first."
-            )
-        # By default there is no preprocessing.
-        os.symlink(self.txtdir, self.srcdir)
+        if not path.isdir(self.srcdir):
+            info("Preprocessing not set, using symlink...")
+            if not path.isdir(self.txtdir):
+                error(
+                    f"{self.txtdir} doesn't exist yet, please download the dataset first."
+                )
+            # By default there is no preprocessing.
+            os.symlink(self.txtdir, self.srcdir)
 
     def mkdir_if_doesnt_exist(self, dir):
         if not path.isdir(dir):
@@ -63,17 +95,19 @@ class DataSet:
             if not path.isdir(subdir):
                 os.makedirs(subdir)
 
-    def compile_all(self, jobs: int = CORES, on_exit=None):
+    def compile(self, jobs: int = CORES, on_exit=None, sample=100):
         self.mkdir_if_doesnt_exist(self.bindir)
         # Copy the files and do some preprocessing
         files_to_compile: List[Tuple[str, str]] = []
         info("Collecting codes to compile")
         for i in tqdm(os.listdir(self.srcdir)):
             for p in os.listdir(path.join(self.srcdir, str(i))):
+                if path.isdir(os.path.abspath(p)):
+                    warning(f"{i}/{p} is a dir, is the dataset correct?")
                 p = p[:-4]
                 src_path = path.join(self.srcdir, str(i), str(p) + ".cpp")
                 bin_path = path.join(self.bindir, str(i), str(p))
-                if not path.isfile(bin_path):
+                if not path.isfile(bin_path) and coin_toss(sample):
                     files_to_compile.append((src_path, bin_path))
 
         info("Compiling all the code")
@@ -97,6 +131,39 @@ class DataSet:
             error("language not supported for removing comments")
 
         return re.sub(pattern, replacer, text)
+
+    # TODO
+    def fuzz(
+        self, jobs: int = CORES, timeout=60, seeds="seeds", on_exit=None, sample=100
+    ):
+        """
+        Fuzz the program
+        """
+        self.mkdir_if_doesnt_exist(self.outdir)
+        bins_to_fuzz: List[Tuple[str, str]] = []
+        info("Collecting binaries to fuzz")
+        for i in tqdm(os.listdir(self.bindir)):
+            for p in os.listdir(path.join(self.bindir, str(i))):
+                if path.isdir(os.path.abspath(p)):
+                    warning(f"{i}/{p} is a dir, is the dataset correct?")
+                bin_path = path.join(self.bindir, str(i), str(p))
+                out_path = path.join(self.outdir, str(i), str(p))
+                if not path.isfile(bin_path) and coin_toss(sample):
+                    bins_to_fuzz.append((bin_path, out_path))
+
+        seeds = path.abspath(seeds)
+        info(f"Fuzzing all {len(bins_to_fuzz)} binaries")
+        parallel_subprocess(
+            bins_to_fuzz,
+            jobs,
+            lambda r: fuzz_one_file(r, timeout=timeout, seeds=seeds),
+            on_exit,
+        )
+
+    def postprocess(self, jobs: int = CORES, on_exit=None, sample=100):
+        """
+        Run the program with fuzzing inputs
+        """
 
 
 class POJ104(DataSet):
@@ -150,5 +217,151 @@ class POJ104(DataSet):
 
 
 class IBM(DataSet):
+    SUBSET = ["C++1000", "C++1400", "Python", "Java"]
+
+    def __init__(self, workdir, subset):
+        self.subset = subset
+        if subset not in IBM.SUBSET:
+            error(f"Incorrect subset given, only {IBM.SUBSET} allowed.")
+            exit(1)
+        DataSet.__init__(self, workdir, self.get_subset_name(), "C/C++")
+
+    def get_subset_name(self):
+        return f"Project_CodeNet_{self.subset}"
+
+    def get_tar_name(self):
+        return f"{self.get_subset_name()}.tar.gz"
+
+    def get_download_path(self):
+        return f"https://dax-cdn.cdn.appdomain.cloud/dax-project-codenet/1.0.0/{self.get_tar_name()}"
+
     def download(self):
-        pass
+        if not os.path.isdir(self.workdir):
+            os.mkdir(self.workdir)
+        else:
+            warning(f"{self.workdir} already exists")
+
+        tar_path = path.join(self.workdir, self.get_tar_name())
+        if not os.path.isfile(tar_path):
+            info("Downloading dataset...")
+            url = self.get_download_path()
+            response = requests.get(url, stream=True)
+            # Download with a nice looking progress bar
+            # https://stackoverflow.com/questions/37573483/progress-bar-while-download-file-over-http-with-requests
+            total_size_in_bytes = int(response.headers.get("content-length", 0))
+            block_size = 1024
+            progress_bar = tqdm(total=total_size_in_bytes, unit="iB", unit_scale=True)
+            with open(tar_path, "wb") as file:
+                for data in response.iter_content(block_size):
+                    progress_bar.update(len(data))
+                    file.write(data)
+            progress_bar.close()
+        else:
+            info("tar already exists.")
+
+        if not os.path.isdir(self.txtdir):
+            info("Extracting dataset, may take a while...")
+            with tarfile.open(tar_path) as tar:
+                # Go over each member
+                for m in tqdm(tar.getmembers()):
+                    # Extract member
+                    tar.extract(member=m, path=self.workdir)
+            info("Extraction done.")
+        else:
+            info("Extracted dataset already exists.")
+
+
+def main():
+    logging.basicConfig()
+    logging.getLogger().setLevel(logging.INFO)
+
+    parser = argparse.ArgumentParser(description="Build a dataset")
+    parser.add_argument(
+        "-d",
+        "--dataset",
+        type=str,
+        choices=["POJ104", "C++1400", "C++1000"],
+        required=True,
+        help="The dataset to copmile",
+    )
+    parser.add_argument(
+        "-w", "--workdir", type=str, default="", help="The workdir to use."
+    )
+    parser.add_argument(
+        "-j", "--jobs", type=int, help="Number of threads to use.", default=CORES
+    )
+    parser.add_argument(
+        "-f", "--errfile", type=str, help="The file name to dump stderr", default="O"
+    )
+    parser.add_argument(
+        "-p",
+        "--pipeline",
+        type=str,
+        help="The stage of the job to run",
+        default="all",
+        choices=["all", "download", "preprocess", "compile", "fuzz", "postprocess"],
+    )
+    parser.add_argument(
+        "-s",
+        "--sample",
+        type=float,
+        help="Only compile and fuzz on a small percentage of dataset (0~100).",
+        default=100.0,
+    )
+    parser.add_argument(
+        "-t", "--time", type=str, help="Time to fuzz one program", default="1m"
+    )
+    parser.add_argument(
+        "-i", "--seeds", type=str, help="Seeds to initialize fuzzing", default="seeds"
+    )
+
+    args = parser.parse_args()
+    workdir = args.workdir if args.workdir != "" else args.dataset
+
+    if path.exists(workdir):
+        warning(f"{workdir} exists")
+
+    if args.sample <= 0 or args.sample > 100:
+        error("Sample rate has to be confined between 1 and 100")
+
+    dataset = None
+    if args.dataset == "POJ104":
+        dataset = POJ104(workdir)
+    elif args.dataset == "C++1400":
+        dataset = IBM(workdir, args.dataset)
+    elif args.dataset == "C++1000":
+        dataset = IBM(workdir, args.dataset)
+    else:
+        unreachable("No dataset provided.")
+
+    def convert_to_seconds(s: str) -> int:
+        seconds_per_unit = {"s": 1, "m": 60, "h": 3600, "d": 86400, "w": 604800}
+        return int(s[:-1]) * seconds_per_unit[s[-1]]
+
+    args.time = convert_to_seconds(args.time)
+
+    if args.pipeline == "all":
+        dataset.download()
+        dataset.preprocess_all()
+        dataset.compile(jobs=args.jobs, sample=args.sample)
+        dataset.fuzz(jobs=args.jobs, timeout=args.time, seeds=args.seeds)
+    elif args.pipeline == "download":
+        dataset.download()
+    elif args.pipeline == "preprocess":
+        dataset.preprocess_all()
+    elif args.pipeline == "compile":
+        dataset.compile(jobs=args.jobs, sample=args.sample)
+    elif args.pipeline == "fuzz":
+        dataset.fuzz(
+            jobs=args.jobs, timeout=args.time, seeds=args.seeds, sample=args.sample
+        )
+    elif args.pipeline == "postprocess":
+        dataset.postprocess(jobs=args.jobs, sample=args.sample)
+    else:
+        unreachable("Unkown pipeline provided")
+
+
+if __name__ == "__main__":
+    logging.basicConfig()
+    logging.getLogger().setLevel(logging.INFO)
+    main()
